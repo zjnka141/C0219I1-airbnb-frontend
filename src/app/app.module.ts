f406423a332fea735105aeb0a6bb62e67aabd8b4
minhnhat1805 <minhnhatdhbkdn@gmail.com>
2019-07-30 14:41:51 +0700

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,21 +16,73 @@ import { NgxGalleryModule } from 'ngx-gallery';
 import { UploadImageComponent } from './components/houses/upload-image/upload-image.component';
 import { RegisterComponent } from './components/accounts/register/register.component';
 import { SocialLoginModule, AuthServiceConfig, FacebookLoginProvider } from "angular-6-social-login";
-import { PostnewsComponent } from './components/houses/postnews/postnews.component';
 import { UpdatePasswordComponent } from './components/accounts/update-password/update-password.component';
-import { ManageHouseComponent } from './components/houses/manage-house/manage-house.component';
-import { httpInterceptorProviders } from './auth/auth-interceptor';
-import { HeaderComponent } from './layout/header/header.component';
-import { ViewHouseComponent } from './components/houses/view-house/view-house.component';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,MatDividerModule,MatGridListModule } 
-from '@angular/material';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FooterComponent } from './layout/footer/footer.component';
-import { HomeComponent } from './layout/home/home.component';
-import { BookingHouseComponent } from './components/houses/booking-house/booking-house.component';
-import { ProfileComponent } from './components/accounts/profile/profile.component';
 import { ListHouseComponent } from './components/houses/list-house/list-house.component';
-import { Ng5SliderModule } from 'ng5-slider';
+import { httpInterceptorProviders } from './auth/auth-interceptor';
+// import { UpdateUserInformationComponent } from './components/accounts/update-user-information/update-user-information.component';
+// import { ListAccountComponent } from './components/accounts/list-account/list-account.component';
+
+
+
+
+import {A11yModule} from '@angular/cdk/a11y';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {PortalModule} from '@angular/cdk/portal';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule,MatInputModule} from '@angular/material';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+
+// import { ListCustomerComponent } from './components/customer/list-customer/list-customer.component';
+import {NgxPaginationModule} from 'ngx-pagination'; 
+// import { MyOrderByPipe } from 'src/app/components/customer/list-customer/sort.pipe';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { OrderModule } from 'ngx-order-pipe';
+import { ModalModule, BsModalRef } from 'ngx-bootstrap/modal';
+
+
+ 
+
+
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
 
 // Configs 
 export function getAuthServiceConfigs() {
@@ -50,22 +102,13 @@ export function getAuthServiceConfigs() {
     LoginComponent,
     UploadImageComponent,
     RegisterComponent,
-    PostnewsComponent,
     UpdatePasswordComponent,
-    ViewHouseComponent,
-    ManageHouseComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    BookingHouseComponent,
-    ProfileComponent,
-    ListHouseComponent
+    ListHouseComponent,
+    // UpdateUserInformationComponent,
+    // ListAccountComponent,
+    // ListCustomerComponent,
+    
   ],
-
-  entryComponents: [
-    UploadImageComponent
-  ],
-
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -79,16 +122,73 @@ export function getAuthServiceConfigs() {
     NgxGalleryModule,
     FormsModule,
     SocialLoginModule,
-    Ng5SliderModule,
-    NgbModule
+
+
+    NgxPaginationModule,Ng2SearchPipeModule,OrderModule, ModalModule.forRoot(),
+
+
+
+    A11yModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    PortalModule,
+    ScrollingModule,
+    MatFormFieldModule,
+    FlexLayoutModule
+    
   ],
   providers: [
     {
       provide: AuthServiceConfig,
       useFactory: getAuthServiceConfigs,
+    
     },
-    httpInterceptorProviders
+    httpInterceptorProviders,
+    // MyOrderByPipe,
+    BsModalRef
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [],
+
 })
 export class AppModule { }
+
+
+platformBrowserDynamic().bootstrapModule(AppModule);
